fe322f035fd7f7004c9230cdcaeeca5c9d4f479d
QUAQUA <quaqua@QUAQUAdeMacBook-Air.local>
2018-04-14 18:20:49 +0800

    Initial Commit

---
+++ ocr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ocr
+//
+//  Created by QUAQUA on 2018/4/14.
+//  Copyright © 2018年 zws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
